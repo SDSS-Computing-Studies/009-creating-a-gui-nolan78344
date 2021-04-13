a4a7043ff38fd2e73e9f75c6bbaba2e8a3416c01
nolano78344 <55156573+nolan78344@users.noreply.github.com>
2021-04-13 10:06:15 -0700

    a

--- task3.py
+++ task3.py
@@ -5,23 +5,23 @@ from tkinter import ttk
 
 window = tk.Tk()
 window.title("Example")
-
+nF = Frame()
 
 dogphoto = PhotoImage(file="dog.png")
 lable1 = tk.Label(window, image=dogphoto, borderwidth=3)
 
 lable2 = tk.Label(window, text="Pochancco!")
 
-
+lable3 = Label(window,text="A cuddly little puppy! This is from the same\n creators that brought you Keropi and Kero Kero", background="#aaffff")
 ###############################################################################
-lable1.grid(row = 1, column = 1)
+lable1.grid(row = 1, column = 2)
 
-lable2.grid(row = 1, column = 2)
+lable2.grid(row = 1, column = 3)
 
-nF = Frame()
-fLabel = Label(nF,text="A cuddly little puppy! This is from the same creators that brought you Keropi and Kero Kero", background="#aaffff")
+lable3.grid(row = 2, column = 1, columnspan = 4)
 
-nF.grid(row = 2, column = 1, columnspan = 2)
+
+
 
 
 window.mainloop()
--- task4.py
+++ task4.py
@@ -2,3 +2,26 @@
 import tkinter as tk 
 from tkinter import *
 from tkinter import ttk
+
+window = tk.Tk()
+window.title("Example")
+nF = Frame()
+
+dogphoto = PhotoImage(file="dog.png")
+lable1 = tk.Label(window, image=dogphoto, borderwidth=3)
+
+lable2 = tk.Label(window, text="Pochancco!")
+
+lable3 = Label(window,text="A cuddly little puppy! This is from the same\n creators that brought you Keropi and Kero Kero", background="#aaffff")
+###############################################################################
+lable1.place(x=50,y=10)
+
+lable2.place(x=120,y=60)
+
+lable3.place(x=-1,y=120)
+
+
+
+
+
+window.mainloop()
